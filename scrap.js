3e77c7ea3f4eaad73cf0a3b4fa27fb1d423027c3
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-01-10 18:49:11 +0200

    testing inserting and getting values from database (MongoDB)
    Co-Authored-By: Tasneem82 <94862430+Tasneem82@users.noreply.github.com>
    Co-Authored-By: mohamed3610 <62805319+mohamed3610@users.noreply.github.com>

--- scrap.js
+++ scrap.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+
 const getInternships = (companyName) => {
     companyName = companyName+" Logo"; 
     return axios
@@ -29,12 +30,49 @@ const getInternships = (companyName) => {
             internship =internship.toString().split(" ")[0, internship.length-1];
             
         }
-        console.log(parsedInternships)
+        // console.log(parsedInternships)
         return  output;    
 
       })
       .catch(console.error);
-    //   console.log(parsedInternships)
   };
   
+// const getInternships = (companyName) =>
+//   {
+//       dataAggregate = [];
+//       // companyName = companyName; 
+//       return axios.get("https://www.glassdoor.com/Job/berlin-software-engineer-internship-jobs-SRCH_IL.0,6_IC2622109_KO7,35.htm")
+//       .then((response) => response.data)
+//       .then( (html) =>
+//       {
+//         const $ = cheerio.load(html);
+//         const internships = $("article > div > ul > li");
+//         internships.each(( i, el) =>
+//         {
+//           const company = $(el)
+//           .find('.d-flex flex-column job-search-key-1pzmdmc e1rrn5ka1')
+//           .end()
+//           .find('.job-search-key-l2wjgv e1n63ojh0 jobLink')
+//           .html()
+
+//           const rating = $(el)
+//           .find(". job-search-key-srfzj0 e1cjmv6j0")
+//           .end();
+         
+
+//           console.log(company);
+//           if (companyName ===company )
+//             {
+//               dataAggregate.push(
+//               {
+//                 rating : rating ,
+//                 companyName : companyName
+//               });
+//             }
+//         });
+        
+//         return dataAggregate;   // .catch(console.error);
+//       }); 
+    // return dataAggregate;   // .catch(console.error);
+  // };
   module.exports = { getInternships };
--- unit/internships.spec.js
+++ unit/internships.spec.js
@@ -1,6 +1,7 @@
 const mockInternshipsApiStub = require("./internships.api.stub.json");
 const { getInternships } = require("../scrap");
 const { mongoClient } = require('../db.js');
+const {MongoClient} = require('mongodb');
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -8,21 +9,46 @@ beforeEach(() => {
 });
 
 describe("Test Internships API", () => {
+
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect("mongodb+srv://solace:nmotkky@cluster0.nyepp.mongodb.net/test", {
+      useNewUrlParser: true,
+    });
+    db = await connection.db("testing");
+  });
+
+  
+  test('should insert a doc into collection', async () => {
+    const glassdoor = db.collection('glassdoor');
+
+    const mockInternship =  await getInternships("Teralytics");
+    await glassdoor.insertOne({companyName:mockInternship});
+
+    const insertedIntrenship = await glassdoor.findOne({companyName:"Teralytics Logo"});
+    expect(insertedIntrenship.companyName).toEqual(mockInternship);
+  });
+  
+  afterAll(async () => {
+    await connection.close();
+    // await db.close();
+  });
+
   test("Test Get Internships", async () => {
     const results = await getInternships("Teralytics");
     expect(results).toEqual(mockInternshipsApiStub);
   });
 
-  test("Test data aggregate get method", async () => {
-    const connectSpy = jest.spyOn(mongoClient, 'connect').mockReturnValueOnce({});
-      const actual = await Common.connect('mongodb://localhost:27017');
-      expect(actual).toEqual({});
-      expect(connectSpy).toBeCalledWith('mongodb://localhost:27017', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      
-  });
-}
-);
+  // test("Test data aggregate get method", async () => {
+  //   const connectSpy = jest.spyOn(mongoClient, 'connect').mockReturnValueOnce({});
+  //     const actual = await Common.connect('mongodb://localhost:27017');
+  //     expect(actual).toEqual({});
+  //     expect(connectSpy).toBeCalledWith('mongodb://localhost:27017', {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true 
+  // });
+// });
 
 });
