8ae42c75e49005261476a5a014da4e7a8f8a8f4d
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-01-10 19:01:53 +0200

    unit and scraping
    Co-Authored-By: Youssef El Zayat <82692972+youssefelzayat@users.noreply.github.com>

---
+++ scrapNew.js
@@ -0,0 +1,58 @@
+const cheerio = require('cheerio');
+const express = require('express');
+const axios = require('axios');
+const {
+    mongoClient
+} = require('./dataAgg');
+const PORT = 3000;
+const app = express();
+
+const getJobs = async (companyName) => {
+    var items = []
+    return axios
+        .get(
+            "https://www.snagajob.com/search?q=software+engineer&radius=20"
+        )
+        .then((response) => response.data)
+        .then((html) => {
+            const $ = cheerio.load(html);
+
+            const jobs = $("job-card > div > div > h2");
+
+            const parsedJobs = [];
+            for (let job of jobs) {
+                parsedJobs.push($(job).text());
+            }
+            const companies = $("job-card > div > div > h3");
+
+            const parsedCompanies = [];
+            for (let company of companies) {
+                parsedCompanies.push($(company).text());
+            }
+
+            for (let i = 0; i < parsedCompanies.length; i++) {
+                items.push([parsedJobs[i], parsedCompanies[i]]);
+            }
+            console.log(items);
+
+            if (companyName) {
+                let searchRes = []
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i][1] == companyName) {
+                        searchRes.push(items[i][0]);
+                    }
+                }
+                console.log(searchRes);
+                return searchRes;
+            }
+            return parsedCompanies;
+        })
+        .catch(console.error);
+
+};
+
+
+
+module.exports = {
+    getJobs
+};
---
+++ unit/snagajob/jobs.spec.js
@@ -0,0 +1,18 @@
+const mockJobsApiStub = require("./jobs.api.stub.json");
+const {
+  getJobs
+} = require("../scrapNew");
+
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+});
+
+describe("Test Jobs API", () => {
+  test("Test Get Jobs", async () => {
+    const results = await getJobs("Software Engineering Services");
+    expect(results).toEqual(mockJobsApiStub);
+  });
+
+});
