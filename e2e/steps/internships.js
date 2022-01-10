a3d1b6d638cbc96f684227e8077f8db50b53e594
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-01-11 00:08:34 +0200

    idk
    Co-Authored-By: Tasneem82 <94862430+Tasneem82@users.noreply.github.com>
    Co-Authored-By: mohamed3610 <62805319+mohamed3610@users.noreply.github.com>

--- e2e/steps/internships.js
+++ e2e/steps/internships.js
@@ -8,13 +8,13 @@ Given("a String {}", function (companyName) {
 
 When("I send a GET request to fetch internships", async function () {
   const {data} = await axios
-    .get(`http://127.0.0.1::3000/internships?string=${this.context["companyName"]}`)
+    .get(`http://localhost:3000/internships?string=${this.context["companyName"]}`)
 this.context["response"] = data
 });
 
 Then("I should get {int} of internships", function (expectedResponse) {
-  console.log("you can't see me"+this.context["response"].length);
-  console.log("my time is now"+expectedResponse);
+  // console.log("you can't see me"+this.context["response"].length);
+  // console.log("my time is now"+expectedResponse);
 
   assert.equal(this.context["response"].length, expectedResponse);
 });
